fcf616ee5083730a6c2c7e3f3eeb4cfed69ac332
Adrian Tosca <adrian.tosca@tradeshift.com>
2021-01-22 17:04:18 +0200

    Add BoardView

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.scss';
 import React from 'react';
+import {BoardView} from "./board/BoardView";
 
 function App() {
     return (
         <div className="App">
             <h1>Otrio</h1>
-
+            <BoardView />
         </div>
   );
 }
---
+++ app/src/board/BoardView.tsx
@@ -0,0 +1,14 @@
+import './BoardView.scss';
+import React from "react";
+
+type Props = {
+
+}
+
+export const BoardView = ({}: Props) => {
+    return (
+        <div className="board">
+            board
+        </div>
+    )
+}
