94c6e6ffaa83889cd5dc228149d209811ecbe5af
Adrian Tosca <adrian.tosca@tradeshift.com>
2021-01-22 14:58:36 +0200

    Initial setup

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Otrio</h1>
+
+        </div>
+  );
+}
+
+export default App;
